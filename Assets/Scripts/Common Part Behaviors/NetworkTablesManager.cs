09b5130f78307063531c8fde97c02c9b4f96c9b3
Bill Kinahan <kinahawi@yahoo.com>
2020-12-15 14:08:59 -0500

    Update for port to MacOS

--- Assets/Scripts/Common Part Behaviors/NetworkTablesManager.cs
+++ Assets/Scripts/Common Part Behaviors/NetworkTablesManager.cs
@@ -12,8 +12,6 @@ public class NetworkTablesManager : MonoBehaviour
     private static bool instanceCreated = false ;
     public static NetworkTableInstance inst = null ;
 
-    public string serverLocation ;
-
 
     // Start is called before the first frame update
     void Start()
@@ -26,7 +24,9 @@ public class NetworkTablesManager : MonoBehaviour
             if ( !instanceCreated ) {
                 inst = NetworkTableInstance.Default ;
                 // wpk probably should make this a parameter
-                NetworkTablesManager.inst.StartClient("192.168.68.109") ;
+                //NetworkTablesManager.inst.StartClient("192.168.68.100");
+                NetworkTablesManager.inst.StartClient("127.0.0.1");
+                //                NetworkTablesManager.inst.StartClient(serverLocation);
                 //Debug.Log($"started network tables client at {serverLocation}") ;
             }
 
